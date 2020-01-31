f8fe93cd9e604c1f78cb04ac00cb236153cb7852
tangtanglove <dai_hang_love@126.com>
2020-01-31 19:32:01 +0800

    提交代码

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import { Dispatch } from 'redux';
-import { Dropdown, Avatar, Layout, Menu, Spin } from 'antd';
+import { Dropdown, Avatar, Menu, Spin } from 'antd';
+import ProLayout, {
+  PageHeaderWrapper
+} from '@ant-design/pro-layout';
 import router from 'umi/router';
-import DocumentTitle from 'react-document-title';
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
@@ -20,9 +22,6 @@ import {
   LogoutOutlined
 } from '@ant-design/icons';
 import styles from './AdminLayout.less';
-
-const { SubMenu } = Menu;
-const { Header, Sider, Content, Footer } = Layout;
 
 interface IProps {
   dispatch:Dispatch<any>;
@@ -45,106 +44,12 @@ class AdminLayout extends Component<IProps> {
     userInfo:{
       nickname:'tangtanglove',
       avatar:undefined
-    }
-  };
-
-  onMenuCollapse = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
+    },
   };
 
   onMenuClick = (e:any) => {
-    const { menus } = this.props;
-
-    let path = '';
-
-    menus.map((menu:any) => {
-      if(menu.children) {
-        menu.children.map((child:any) => {
-          if(child.children) {
-            child.children.map((grandson:any) => {
-              if(e.key == grandson.id) {
-                path = grandson.path;
-              }
-            })
-          } else {
-            if(e.key == child.id) {
-              path = child.path;
-            }
-          }
-        })
-      } else {
-        if(e.key == menu.id) {
-          path = menu.path;
-        }
-      }
-    })
-
-    router.push(path);
+    router.push(e.key);
   };
-
-  menuIcon = (icon:string) =>{
-    if(icon == 'home' || icon == 'HomeOutlined') {
-      return(
-        <HomeOutlined />
-      )
-    }
-
-    if(icon == 'file-word' || icon == 'FileWordOutlined') {
-      return(
-        <FileWordOutlined />
-      )
-    }
-
-    if(icon == 'file-ppt' || icon == 'FilePptOutlined') {
-      return(
-        <FilePptOutlined />
-      )
-    }
-
-    if(icon == 'user-add' || icon == 'UserAddOutlined') {
-      return(
-        <UserAddOutlined />
-      )
-    }
-
-    if(icon == 'usergroup-add' || icon == 'UsergroupAddOutlined') {
-      return(
-        <UsergroupAddOutlined />
-      )
-    }
-
-    if(icon == 'snippets' || icon == 'SnippetsOutlined') {
-      return(
-        <SnippetsOutlined />
-      )
-    }
-
-    if(icon == 'setting' || icon == 'SettingOutlined') {
-      return(
-        <SettingOutlined />
-      )
-    }
-
-    if(icon == 'paper-clip' || icon == 'PaperClipOutlined') {
-      return(
-        <PaperClipOutlined />
-      )
-    }
-
-    if(icon == 'user' || icon == 'UserOutlined') {
-      return(
-        <UserOutlined />
-      )
-    }
-
-    if(icon == 'shop' || icon == 'ShopOutlined') {
-      return(
-        <ShopOutlined />
-      )
-    }
-  }
 
   render() {
 
@@ -166,112 +71,43 @@ class AdminLayout extends Component<IProps> {
     );
 
     return (
-      <DocumentTitle
-        title={'后台管理'}
+      <div
+        style={{
+          transform: 'rotate(0)',
+          overflowX: 'hidden',
+        }}
       >
-        <Layout>
-          <Sider trigger={null} collapsible collapsed={this.state.collapsed}
-            style={{
-              overflow: 'auto',
-              height: '100vh',
-              position: 'fixed',
-              left: 0,
-            }}
-          >
-            <div className={styles.logo}>Logo</div>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-              {!!menus &&
-                menus.map((menu:any) => {
-                  if(menu.children) {
-                    return (
-                      <SubMenu key={menu.id}
-                        title={
-                          <span>
-                            {this.menuIcon(menu.icon)}
-                            <span>{menu.name}</span>
-                          </span>
-                        }
-                      >
-                        {!!menu.children && menu.children.map((grandson:any) => {
-                          if(grandson.children) {
-                            return (
-                              <SubMenu key={grandson.id}
-                                title={
-                                  <span>
-                                    {this.menuIcon(grandson.icon)}
-                                    <span>{grandson.name}</span>
-                                  </span>
-                                }
-                              >
-                                {!!grandson.children && grandson.children.map((grandsonItem:any) => {
-                                  return(
-                                    <Menu.Item key={grandsonItem.id} onClick={this.onMenuClick} >
-                                      {this.menuIcon(grandsonItem.icon)}
-                                      <span>{grandsonItem.name}</span>
-                                    </Menu.Item>
-                                  )
-                                })}
-                              </SubMenu>
-                            )
-                          } else {
-                            return (
-                              <Menu.Item key={grandson.id} onClick={this.onMenuClick} >
-                                {this.menuIcon(grandson.icon)}
-                                <span>{grandson.name}</span>
-                              </Menu.Item>
-                            )
-                          }
-                        })}
-                      </SubMenu>
-                    );
-                  } else {
-                    return (
-                      <Menu.Item key={menu.id}>
-                        {this.menuIcon(menu.icon)}
-                        <span>{menu.name}</span>
-                      </Menu.Item>
-                    );
-                  }
-                })
+        <ProLayout
+          style={{
+            height:'100vh',
+          }}
+          location={{
+            pathname: '/console/index?id=1',
+          }}
+          menuDataRender={() => menus}
+          fixedHeader={true}
+          fixSiderbar={true}
+          rightContentRender={() =>
+            <div className={styles.right}>
+              {this.state.userInfo.nickname ? 
+                <Dropdown className={styles.container} overlay={menu}>
+                  <span className={`${styles.action} ${styles.account}`}>
+                    <Avatar size="small" className={styles.avatar} src={this.state.userInfo.avatar} alt="avatar" />
+                      <span className={styles.name}>{this.state.userInfo.nickname}</span>
+                  </span>
+                </Dropdown>
+              : 
+                <Spin size="small" style={{ marginLeft: 8, marginRight: 8 }} />
               }
-            </Menu>
-          </Sider>
-          <Layout className={styles.siteLayout} style={{ marginLeft: 200 }}>
-            <Header className={styles.siteLayoutBackground} style={{ padding: 0, position: 'fixed', zIndex: 1, width: '100%' }}>
-              {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                className: styles.trigger,
-                onClick: this.onMenuCollapse,
-              })}
-              <div className={styles.right}>
-                {this.state.userInfo.nickname ? 
-                  <Dropdown className={styles.container} overlay={menu}>
-                    <span className={`${styles.action} ${styles.account}`}>
-                      <Avatar size="small" className={styles.avatar} src={this.state.userInfo.avatar} alt="avatar" />
-                        <span className={styles.name}>{this.state.userInfo.nickname}</span>
-                    </span>
-                  </Dropdown>
-                : 
-                  <Spin size="small" style={{ marginLeft: 8, marginRight: 8 }} />
-                }
-              </div>
-            </Header>
-            <Content
-              className={styles.siteLayoutBackground}
-              style={{
-                marginTop:80,
-                marginBottom:24,
-                marginLeft:16,
-                marginRight:16,
-                padding: 24,
-                overflow: 'initial'
-              }}
-            >
-              {children}
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>FullStack ©2020</Footer>
-          </Layout>
-        </Layout>
-      </DocumentTitle>
+            </div>
+          }
+          menuProps={{ onClick:this.onMenuClick}}
+        >
+          <PageHeaderWrapper content="欢迎使用">
+            {children}
+          </PageHeaderWrapper>
+        </ProLayout>
+      </div>
     );
   }
 }
